585a70b39d1ab72391de4c062679e13e9665150a
ogontaro <ogontaro@gmail.com>
2024-10-07 23:38:33 +0900

    update template

--- subtree-update.sh
+++ subtree-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-git subtree split --prefix=templates/typescript_nodejs -b update
-git push git@github.com:ogontaro/templates_typescript_nodejs.git update:main
-git branch -d update
+git subtree split --prefix=templates/template -b update
+git push git@github.com:ogontaro/templates_template.git update:main
+git branch -D update
